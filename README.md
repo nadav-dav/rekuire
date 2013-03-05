8960a9d346a3e30bc4891d4d2f961132c4024292
Nadav L <nadavl@wix.com>
2013-03-06 01:05:09 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Rekuire
+=========
+> 'rekuire' is basically node's 'require' without the relative paths
+
+installation
+-------------
+to install, type
+>```npm install req
+```
+
+
+so how to use it?
+-----------------
+> instead of doing this:
+> ```var MyModule = require('../../../MyModule.js');``` *<-- yuck!*
+> &nbsp;
+> why not do this:
+> ```var rekuire = require('rekuire');```
+> ```var MyModule = rekuire('MyModule.js');```
+
+
+what does it do?
+----------------
+> when 'rekuire' is first loaded to the project, it scans the **.js* files,
+> then, when you need them they are right there to use!
+> no relative paths are needed! *yeahy!*
---
+++ test/rekuire.spec.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var fs = require('fs-extra');
+
+var base = __dirname + "/../";
+
+describe("Testing 'rekuire'",function(){
+
+    beforeEach(function(){
+        var setupDone = false;
+        runs( function () {
+            copyReqToNodeModules(function(){
+                setupDone = true;
+            });
+        });
+        waitsFor(function () { return !!setupDone; } , 'Timed out', 100);
+    });
+
+    afterEach(function(){
+        clearNodeModules();
+    });
+
+
+
+    describe("when running",function(){
+        it("should retrieve it according to the file name",function(){
+            runs(function(){
+                var rekuire = require('rekuire');
+                var imported = rekuire('someModule.js');
+                expect(imported).toBe("some module");
+            });
+        });
+
+        it("should retrieve it according to relative path",function(){
+            runs(function(){
+                var rekuire = require('rekuire');
+                var imported = rekuire('./testResources/nestedPackage/someModule.js');
+                expect(imported).toBe("some module");
+            });
+        });
+
+        it("should retrieve module from node_modules",function(){
+            runs(function(){
+                var rekuire = require('rekuire');
+                var fse = rekuire('fs-extra');
+                expect(fse).not.toBeNull();
+            });
+        });
+
+        it("should retrieved node framework modules",function(){
+            runs(function(){
+                var rekuire = require('rekuire');
+                var fse = rekuire('fs');
+                expect(fse).not.toBeNull();
+            })
+        });
+    });
+});
+
+
+
+
+// SETUP & TEARDOWN
+
+function copyReqToNodeModules(callback){
+    fs.mkdirsSync( base + "/node_modules/rekuire/lib");
+    fs.copy(base+"lib/", base + "/node_modules/rekuire/lib", function(){
+        fs.copy(base+"package.json", base + "node_modules/rekuire/package.json", callback);
+    });
+}
+
+function clearNodeModules(){
+    fs.remove( base + "node_modules/rekuire");
+}
